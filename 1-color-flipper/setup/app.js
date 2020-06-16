9d8f03e9a168054afa245ba3d5822be39784b8b0
kola192 <muwakil96@gmail.com>
2020-06-17 00:28:49 +0200

    the funtionality works properly

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
+// setting variables
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const colorSpan = document.querySelector('.color');
+
+//adding event listener to the button
+btn.addEventListener('click', () => {
+	ranomNumber = Math.floor(Math.random() * colors.length);
+
+	colorSpan.textContent = colors[ranomNumber];
+	document.body.style.backgroundColor = colors[ranomNumber];
+});
+
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,22 @@
+// setting variables
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const colorSpan = document.querySelector('.color');
+
+//adding event listener to the button
+btn.addEventListener('click', () => {
+	let hexColor = '#';
+	//looping in hex array 6 times to make the color
+	for(let i=0;i<6;i++) {
+		hexColor += hex[getRandomNumber()];
+		}
+
+	colorSpan.textContent = hexColor;
+	document.body.style.backgroundColor = hexColor;
+});
+
+//getRandomNumber function
+function getRandomNumber() {
+	return Math.floor(Math.random() * hex.length);
+};
